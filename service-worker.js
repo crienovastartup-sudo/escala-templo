73280568effadcec1960e49323a0d5736839eb11
crienovastartup-sudo <crienovastartup@gmail.com>
2026-02-27 13:31:56 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,9 @@
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("escala").then(c=>c.addAll(["./","index.html"]))
+  );
+});
 
+self.addEventListener("fetch", e => {
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+});
